affe8d2dc88b9800c1d3924054ca12f304567d44
Leslie Reyes <l.reyes.ubiqum@gmail.com>
2020-08-21 12:20:33 +0200

    SideDrawer mobile

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,8 +1,7 @@
 import React from "react";
-import navigationItems from "../NavigationItems";
 import classes from "./NavigationItem.module.css";
 
-const navigationItem = (props) = (
+const navigationItem = (props) => (
   <li className={classes.NavigationItem}>
     <a href={props.link} className={props.active ? classes.active : null}>
       {props.children}
